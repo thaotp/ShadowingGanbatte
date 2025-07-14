3358bb2a4620d2bf7273d4943f9d19fe45ffd571
TungDT32 <tungdt32@fsoft.com.vn>
2025-07-15 08:28:58 +0900

    Initial Commit

---
+++ ShadowingGabatte/ShadowingGabatteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShadowingGabatteApp.swift
+//  ShadowingGabatte
+//
+//  Created by TungNS on 2025/07/15.
+//
+
+import SwiftUI
+
+@main
+struct ShadowingGabatteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShadowingGabatteTests/ShadowingGabatteTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShadowingGabatteTests.swift
+//  ShadowingGabatteTests
+//
+//  Created by TungNS on 2025/07/15.
+//
+
+import Testing
+@testable import ShadowingGabatte
+
+struct ShadowingGabatteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
